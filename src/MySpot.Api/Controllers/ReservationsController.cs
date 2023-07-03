e401742ad0a8a9b22d2a317378cb60f59149cc8f
Mateusz <matzaj101@gmail.com>
2023-07-04 00:14:27 +0200

    init

---
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySpot.Api.NewFolder;
+using MySpot.Api.Services;
+
+namespace MySpot.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ReservationsController: ControllerBase 
+    {
+        //controller can hold any state, every request start from new state so static keyword need to be added
+        private readonly ReservationsService _service = new ReservationsService();
+
+        [HttpPost]
+        public IActionResult Post(Reservation reservation)
+        {
+           var id = _service.Create(reservation);
+            if(id == null)
+            {
+                return BadRequest();
+            }
+           return CreatedAtAction(nameof(Get), new {id = reservation.Id}, reservation);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Reservation>> Get() => Ok(_service.GetAll());
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Reservation> Get(int id)
+        {
+            var reservation = _service.Get(id);
+            if(reservation == null)
+            {
+                return NotFound();
+            }
+          
+            return Ok(reservation);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Reservation reservation)
+        {
+            if(_service.Update(id, reservation))
+            {
+                return NoContent();
+            }
+            return NotFound();
+            
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+           if (_service.Delete(id))
+           {
+                return NoContent();
+           }
+           return NotFound();
+        }
+    }
+}
---
+++ src/MySpot.Api/Services/ReservationsService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MySpot.Api.NewFolder;
+
+namespace MySpot.Api.Services
+{
+    public class ReservationsService
+    {
+        private static readonly List<Reservation> _reservations = new();
+        private static int _id = 1;
+        private static readonly List<string> _parkingSpotName = new() { "P1", "P2", "P3", "P4", "P5" };
+
+        public Reservation Get(int id)=> _reservations.SingleOrDefault(x => x.Id == id);
+          
+        public IEnumerable<Reservation> GetAll() => _reservations;
+
+        public int? Create(Reservation reservation)
+        {
+            if (_parkingSpotName.All(x => x != reservation.ParkingSpotName))
+            {
+                return null;
+            }
+            reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+
+            var reservationAlreadyExist = _reservations.Any(x =>
+                x.ParkingSpotName == reservation.ParkingSpotName &&
+                x.Date.Date == reservation.Date.Date);
+
+            if (reservationAlreadyExist)
+            {
+                return null;
+            }
+
+            reservation.Id = _id;
+
+            _id++;
+            _reservations.Add(reservation);
+            return reservation.Id;
+        }
+
+        public bool Update(int id, Reservation reservation)
+        {
+            var existingReservation = _reservations.SingleOrDefault(x => x.Id == id);
+            if (existingReservation == null)
+            {
+                return false;
+            }
+
+            existingReservation.LicensePlate = reservation.LicensePlate;
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var reservation = _reservations.SingleOrDefault(x => x.Id == id);
+            if (reservation == null)
+            {
+                return false;
+            }
+            _reservations.Remove(reservation);
+            return true;
+        }
+    }
+}
