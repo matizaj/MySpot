ce45ec64440b33c39e1deb8185ada7a32c8f85bf
Mateusz Zajac <mateusz.zajac@pmi.com>
2023-10-02 21:21:01 +0200

    query user by email

---
+++ src/MySpot.Infrastructure/MySpot.Infrastructure/DAL/Handlers/GetUserByEmailHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using MySpot.Application.Abstractions;
+using MySpot.Application.Dtos;
+using MySpot.Application.Queries;
+using MySpot.Core.Repositories;
+
+namespace MySpot.Infrastructure.DAL.Handlers
+{
+    public class GetUserByEmailHandler : IQueryHandler<GetUserByEmail, UserDto>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByEmailHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<UserDto> HandleAsync(GetUserByEmail query)
+        {
+            var user = await _userRepository.GetUserByEmail(query.Email);
+            return user.AsDto();
+        }
+    }
+}
+
