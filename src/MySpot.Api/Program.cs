67b3e7a7398b33d23941c89d92b6d1bbfbbc8018
Mateusz <matzaj101@gmail.com>
2023-10-06 12:29:10 +0200

    clean imports

--- src/MySpot.Api/Program.cs
+++ src/MySpot.Api/Program.cs
@@ -4,12 +4,6 @@ using MySpot.Application;
 using Serilog;
 using MySpot.Infrastructure.Logging;
 using Microsoft.Extensions.Options;
-using MySpot.Application.Queries;
-using MySpot.Application.Abstractions;
-using MySpot.Application.Dtos;
-using MySpot.Application.Commands;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
-using Microsoft.AspNetCore.Mvc;
 using MySpot.Api;
 
 var builder = WebApplication.CreateBuilder(args);
